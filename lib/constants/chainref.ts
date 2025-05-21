b5c94c13e80245d062e47a8cd4270cae8e1d4512
Samiul Alim <xo.ot2001@gmail.com>
2025-05-21 16:00:12 +0600

    feat: added chainref object

---
+++ lib/constants/chainref.ts
@@ -0,0 +1,29 @@
+const chainref = {
+    eth: 1,
+    bsc: 56,
+    polygon: 137,
+    optimism: 10,
+    arbitrum: 42161,
+    avalanche: 43114,
+    gnosis: 100,
+    fantom: 146,
+    aurora: 1313161554,
+    klaytn: 8217,
+    zksync: 324,
+    base: 8453,
+    mantle: 5000,
+    merlin: 4200,
+    kava: 2222,
+    linea: 59144,
+    celo: 42220,
+    iota_evm: 8822,
+    manta: 169,
+    zetachain: 7000,
+    astar: 592,
+    moonbeam: 1284,
+    eos_evm: 17777,
+    hyperliquid_evm: 999,
+    cronos: 25
+};
+
+export default chainref;
